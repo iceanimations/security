2fbeb5e8013fb228ec266cb614da7619466cda49
Talha Ahmed <talha.ahmed@gmail.com>
2014-11-17 10:57:04 +0500

    Allow VFX to check in to shots

--- checkin_permission.py
+++ checkin_permission.py
@@ -42,12 +42,13 @@ def checkinability(search_key, process = None, context = None, user = None):
                 # modeling and rigging dept can checkin to asset
                 ('/asset' in search_key and
                  any([dpt in login['login_group'] for dpt in ['model',
-                                                              'rig', 
+                                                              'rig',
                                                               'light']])) or
                 # animation dept can checkin to shot
                 ('/shot' in search_key and
                  any([dpt in login['login_group'] for dpt in ['animation',
-                                                                'light']]))):
+                                                                'light',
+                                                                'vfx']]))):
 
                 return True
 
